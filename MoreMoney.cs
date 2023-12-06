e402b29ac4108ffd768f7e07734f0bf1eac73441
Zukuig <139835367+Zukuig@users.noreply.github.com>
2023-12-06 14:17:20 -0800

    Add files via upload

---
+++ MoreMoney.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+
+namespace MoreMoneyStart.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    internal class MoreMoney
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void betterStartingAmount(Terminal __instance, ref int ___groupCredits)
+        {
+            QuotaSettings quotaSettings = new QuotaSettings();
+
+            if (___groupCredits == quotaSettings.startingCredits && TimeOfDay.Instance.daysUntilDeadline == 3 && TimeOfDay.Instance.profitQuota == 130) // isolate to the first day of a new game
+            {
+                ___groupCredits = 10000;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class ResetTerminal
+    {
+        [HarmonyPatch("ResetShip")]
+        [HarmonyPostfix]
+        static void resetTerminal()
+        {
+            UnityEngine.Object.FindObjectOfType<Terminal>().groupCredits = 10000;
+        }
+    }
+}
